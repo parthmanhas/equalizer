a3c2c7e58840b5688dde52c4c6dd19e83a51ada5
ishouldcode <parthmanhas@gmail.com>
2020-04-01 19:43:08 +0530

    Equaliser Home UI Improved

--- App.js
+++ App.js
@@ -6,17 +6,20 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import EqualizerScreen from './src/screens/EqualizerScreen';
 import EffectsScreen from './src/screens/EffectsScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import { MenuProvider } from 'react-native-popup-menu';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Equalizer" component={EqualizerScreen} />
-        <Tab.Screen name="Effects" component={EffectsScreen} />
-        <Tab.Screen name="Profiles" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <MenuProvider>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Equalizer" component={EqualizerScreen} />
+          <Tab.Screen name="Effects" component={EffectsScreen} />
+          <Tab.Screen name="Profiles" component={ProfileScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </MenuProvider>
   );
 }
--- src/screens/EqualizerScreen.js
+++ src/screens/EqualizerScreen.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import Slider from '@react-native-community/slider';
+import {
+    Menu,
+    MenuOptions,
+    MenuOption,
+    MenuTrigger,
+} from 'react-native-popup-menu';
 
 const EqualizerScreen = props => {
     return (
         <View style={styles.screen}>
+            <View style={styles.headerContent}>
+                <View style={styles.headerStyle}>
+                    <Text style={{ padding: 10 }}>Equaliser FX</Text>
+                    <View style={styles.headerMenuButton}>
+                        <Menu>
+                            <MenuTrigger children={<Text style={{ fontSize: 25 }}>...</Text>} />
+                            <MenuOptions>
+                                <MenuOption onSelect={() => {}} text='Settings' />
+                                <MenuOption onSelect={() => {}} text='Reset Profile' />
+                            </MenuOptions>
+                        </Menu>
+                    </View>
+                </View>
+            </View>
             <View style={styles.sliderAnddbTextParentContainer}>
                 <View style={styles.dbTextContainer}>
-                    <Text style={{ paddingHorizontal: 5}}>+15db</Text>
-                    <Text style={{ paddingHorizontal: 5}}>+15db</Text>
-                    <Text style={{ paddingHorizontal: 5}}>+15db</Text>
-                    <Text style={{ paddingHorizontal: 5}}>+15db</Text>
-                    <Text style={{ paddingHorizontal: 5}}>+15db</Text>
+                    <Text style={{ paddingHorizontal: 5 }}>+15db</Text>
+                    <Text style={{ paddingHorizontal: 5 }}>+15db</Text>
+                    <Text style={{ paddingHorizontal: 5 }}>+15db</Text>
+                    <Text style={{ paddingHorizontal: 5 }}>+15db</Text>
+                    <Text style={{ paddingHorizontal: 5 }}>+15db</Text>
                 </View>
                 <View style={styles.sliderContainer}>
                     <Slider
@@ -62,14 +82,14 @@ const styles = StyleSheet.create({
     },
     sliderContainer: {
         // flex: 0.5,
-        // transform: [{ rotate: '90deg' }],
+        transform: [{ rotate: '90deg' }],
         width: 400,
         height: 300,
         justifyContent: 'center',
         alignContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'yellow',
-        marginVertical: 50
+        // backgroundColor: 'yellow',
+        marginVertical: 60
     },
     slider: {
         width: '90%',
@@ -77,16 +97,30 @@ const styles = StyleSheet.create({
     },
     dbTextContainer: {
         flexDirection: "row",
-        backgroundColor: 'grey',
+        // backgroundColor: 'grey',
         alignContent: 'center',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        padding: 10
         // transform: [{ rotate: '90deg' }]
     },
-    sliderAnddbTextParentContainer:{
-        flex : 1,
-        backgroundColor: 'red',
-        justifyContent: 'flex-end'
+    sliderAnddbTextParentContainer: {
+        flex: 1,
+        // backgroundColor: 'red',
+        justifyContent: 'center',
+        // backgroundColor: 'blue'
+    },
+    headerContent: {
+        padding: 20,
+        // backgroundColor: 'grey'
+    },
+    headerStyle: {
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        // backgroundColor: 'yellow'
+    },
+    headerMenuButton: {
+        transform: [{ rotate: '90deg' }],
     }
 });
 
